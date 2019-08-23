983ee30a6fe1b156e6dca73aa6829d54692c90ef
Jark Wu <wuchong.wc@alibaba-inc.com>
2019-08-24 00:11:37 +0800

    read resource using getResourceAsStream

--- src/main/java/com/github/wuchong/flink/notification/bot/util/ResourceUtils.java
+++ src/main/java/com/github/wuchong/flink/notification/bot/util/ResourceUtils.java
@@ -18,21 +18,19 @@
 
 package com.github.wuchong.flink.notification.bot.util;
 
-import com.google.common.io.Files;
-
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class ResourceUtils {
 
     public static String readResourceFile(String fileName) {
-        File file = new File(ResourceUtils.class.getClassLoader().getResource(fileName).getFile());
-        List<String> line = null;
-        try {
-            line = Files.readLines(file, StandardCharsets.UTF_8);
-            return String.join("\n", line);
+        try (InputStream inputStream = ResourceUtils.class.getClassLoader().getResourceAsStream(fileName)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder sb = new StringBuilder();
+            return reader.lines().collect(Collectors.joining("\n"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
